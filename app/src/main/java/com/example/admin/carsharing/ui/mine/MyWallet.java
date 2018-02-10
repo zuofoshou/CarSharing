821d63a080845ad9de69e761a3139e625772e0eb
zuofoshou <litao104@foxmail.com>
2018-02-10 21:42:42 +0800

    我的钱包

---
+++ app/src/main/java/com/example/admin/carsharing/ui/mine/MyWallet.java
@@ -0,0 +1,28 @@
+package com.example.admin.carsharing.ui.mine;
+
+import com.example.admin.carsharing.R;
+import com.example.admin.carsharing.ui.BaseAty;
+
+/**
+ * Class Name: 我的钱包
+ * Created by zlt .
+ * 简介：
+ * Data： 2018/2/9.
+ */
+
+public class MyWallet extends BaseAty{
+    @Override
+    public int getLayoutId() {
+        return R.layout.mine_my_wallet;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void requestData() {
+
+    }
+}
